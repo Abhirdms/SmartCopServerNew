d2e01c4249077ac4e4e43eb4e81c02d621175edd
Abhishek giri <abhishekgiri25200@gmail.com>
2024-01-10 15:54:51 +0530

    Delete routers directory

--- routers/signuprouter.js
+++
@@ -1,8 +0,0 @@
-// routes/signuprouter.js
-const express = require('express');
-const signupRouter = express.Router();
-const signupController = require('../controller/signup.controller');
-
-signupRouter.post('api/signup', signupController.signup);
-
-module.exports = signupRouter;
--- routers/userloginrouter.js
+++
@@ -1,8 +0,0 @@
-// routes/userloginrouter.js
-const express = require('express');
-const userLoginRouter = express.Router();
-const userLoginController = require('../controller/userlogin.controller');
-
-userLoginRouter.post('/api/userlogin', userLoginController.login);
-
-module.exports = userLoginRouter;
