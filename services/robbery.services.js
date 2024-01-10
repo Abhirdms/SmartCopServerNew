70b8dcdd9a509b69bed516a0a9727bf3cdec94d9
Abhishek giri <abhishekgiri25200@gmail.com>
2024-01-10 13:10:47 +0530

    Update robbery.services.js

--- services/robbery.services.js
+++ services/robbery.services.js
@@ -4,29 +4,68 @@ const Robbery = require('../model/robbery'); // Your Mongoose model for MongoDB
 
 module.exports = {
   processUpload: async (file) => {
+    return new Promise(async (resolve, reject) => {
     try {
       const results = [];
+      const expectedColumns = [
+
+
+        "Crimeno",
+ "Policestation",  // Assuming it's a Unix timestamp
+  "Occurancedate",
+  "Occurancetime",
+  "Registerdate",
+  "Lat",
+  "Lat",
+ 
+      ];
+      let actualColumns;
       // Process the CSV file
       fs.createReadStream(file.path)
         .pipe(csv())
         .on('data', (data) => {
+
+          if (!actualColumns) {
+            actualColumns = Object.keys(data).map(column => column.toLowerCase());
+          }
+
+          const missingColumns = expectedColumns
+            .filter((column) => !actualColumns.includes(column.toLowerCase()));
+
+          if (missingColumns.length > 0) {
+            const errorMessage = `Missing columns: ${missingColumns.join(', ')}`;
+            console.error(errorMessage);
+            reject(new Error(errorMessage));
+            return;
+          }
+          const transformedData = {};
+          expectedColumns.forEach((column) => {
+            const columnIndex = actualColumns.indexOf(column.toLowerCase());
+            transformedData[column] = data[Object.keys(data)[columnIndex]];
+          });
+
+
+
           // Process each row of CSV data and push it into the results array
-          results.push(data);
+          results.push(transformedData);
         })
         .on('end', async () => {
+
+          if (results.length === 0) {
+            console.error('No valid data found in the CSV file');
+  reject({ success: false, error: new Error('No valid data found in the CSV file') });
+  return;
+          }
           // Save data to the database (assumes Mongoose schema is defined)
           const result = await Robbery.create(results);
-          return { message: 'File uploaded and data saved successfully', data: result };
+          resolve({ success: true, message: 'File uploaded and data saved successfully', data: result });
+        }).on('error', (error) => {
+          console.error('Error during CSV processing:', error);
+          reject(error);
         });
-
-      return new Promise((resolve, reject) => {
-        // Handle any errors during CSV processing
-        process.on('unhandledRejection', (reason, promise) => {
-          reject(reason);
-        });
-      });
     } catch (error) {
       throw error;
     }
+  });
   },
 };
