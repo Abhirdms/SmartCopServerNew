14a8bb50d262ea19cae59109f5fe1205767994ac
Abhishek giri <abhishekgiri25200@gmail.com>
2024-01-10 12:47:28 +0530

    Update dacoity.controller.js 2

--- controller/dacoity.controller.js
+++ controller/dacoity.controller.js
@@ -12,9 +12,9 @@ module.exports = {
       console.log(result);
 
       if (result.success) {
-        return res.status(200).json({ message: 'File uploaded and processed successfully' });
+        return res.status(200).json({ message: result.message, data: result.data });
       } else {
-        return res.status(500).json({ error: 'File upload failed' });
+        res.status(500).json({ error: error.message || 'Internal Server Error' });
       }
     } catch (error) {
       console.error(error);
