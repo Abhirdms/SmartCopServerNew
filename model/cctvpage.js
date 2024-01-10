8bff0c235056533dc5db98ffeed0e847cbb18396
Abhishek giri <abhishekgiri25200@gmail.com>
2024-01-10 12:59:23 +0530

    Create cctvpage.js

---
+++ model/cctvpage.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const cctvSchema = new mongoose.Schema({
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'], // Restrict the type to 'Point'
+      required: true,
+    },
+    coordinates: {
+      type: [Number], // Array of [longitude, latitude] for GeoJSON Point
+      required: true,
+    },
+  },
+  address: String,
+});
+
+// Index the location field for geospatial queries
+cctvSchema.index({ location: '2dsphere' });
+
+module.exports = mongoose.model('Cctvpage', cctvSchema);
