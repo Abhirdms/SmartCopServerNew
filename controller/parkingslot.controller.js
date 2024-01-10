96caad06e428ae9b3bc514d6a03305e328c0829f
Abhishek giri <abhishekgiri25200@gmail.com>
2024-01-10 12:50:30 +0530

    Update parkingslot.controller.js 2

--- controller/parkingslot.controller.js
+++ controller/parkingslot.controller.js
@@ -8,15 +8,17 @@ module.exports = {
       }
 
       const result = await uploadService.processUpload(req.file);
+     
 
       if (result.success) {
         return res.status(200).json({ message: result.message, data: result.data });
       } else {
-        res.status(500).json({ error: error.message || 'Internal Server Error' });
+        return res.status(400).json({ error: result.error.message || 'File upload failed' });
       }
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ error: 'File upload failed' });
+      console.log(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
   },
 };
