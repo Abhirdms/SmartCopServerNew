afe84081077a419cbdb7868c112b8a60980e0024
Abhishek giri <abhishekgiri25200@gmail.com>
2024-01-11 09:58:04 +0530

    Update offenders.service.js 2

--- services/offenders.service.js
+++ services/offenders.service.js
@@ -403,7 +403,7 @@ class OffenderService {
           .pipe(csv())
           .on('data', (data) => {
             if (!actualColumns) {
-              actualColumns = Object.keys(data).map(column => column.toLowerCase());
+              actualColumns = Object.keys(data).map(column => column.toLowerCase().trim());
             }
 
             // Handle missing columns
