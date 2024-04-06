1e071163bcd7cc5a93dc106c766ac8742df68040
mishrakhushi16 <132750159+mishrakhushi16@users.noreply.github.com>
2024-04-06 15:00:38 +0530

    Update theft.controller.js

--- controller/theft.controller.js
+++ controller/theft.controller.js
@@ -9,10 +9,10 @@ module.exports = {
 
       const result = await uploadService.processUpload(req.file);
 
-      if (result.success) {
-        return res.status(200).json({ message: 'File uploaded and processed successfully' });
+      if (result.message) {
+        return res.status(200).json({ message: result.message, data: result.data });
       } else {
-        res.status(500).json({ error: error.message || 'Internal Server Error' });
+        return res.status(500).json({ error: 'File upload failed' });
       }
     } catch (error) {
       console.error(error);
