abc5126214bcd31741ae7640065c19438fd4bca0
mishrakhushi16 <132750159+mishrakhushi16@users.noreply.github.com>
2024-04-06 14:59:03 +0530

    Update dacoity.controller.js

--- controller/dacoity.controller.js
+++ controller/dacoity.controller.js
@@ -11,10 +11,10 @@ module.exports = {
       const result = await uploadService.processUpload(req.file);
       console.log(result);
 
-      if (result.success) {
+     if (result.message) {
         return res.status(200).json({ message: result.message, data: result.data });
       } else {
-        res.status(500).json({ error: error.message || 'Internal Server Error' });
+        return res.status(500).json({ error: 'File upload failed' });
       }
     } catch (error) {
       console.error(error);
