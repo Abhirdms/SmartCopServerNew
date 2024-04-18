4be94125e853897e109486de23cf9ab837d79dfd
mishrakhushi16 <132750159+mishrakhushi16@users.noreply.github.com>
2024-04-18 16:43:54 +0530

    Update cctvpage.controller.js

--- controller/cctvpage.controller.js
+++ controller/cctvpage.controller.js
@@ -10,10 +10,10 @@ module.exports = {
       const result = await uploadService.processUpload(req.file);
       console.log(result);
 
-      if (result.success) {
+      if (result.message) {
         return res.status(200).json({ message: result.message, data: result.data });
       } else {
-        return res.status(400).json({ error: result.error.message || 'File upload failed' });
+        return res.status(500).json({ error: 'File upload failed', details: result.error.message });
       }
     } catch (error) {
       console.error(error);
