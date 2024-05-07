1e499539b426bf0613dc3a44dd2a4c55897d7918
mishrakhushi16 <132750159+mishrakhushi16@users.noreply.github.com>
2024-05-07 15:18:15 +0530

    Update upload.controller.js

--- controller/upload.controller.js
+++ controller/upload.controller.js
@@ -34,11 +34,11 @@ module.exports = {
     if (result.message) {
       return res.status(200).json({ message: result.message, data: result.data });
     } else {
-      return res.status(500).json({ error: 'File upload failed' });
+      return res.status(500).json({ error: error.message , data: error.data});
     }
   } catch (error) {
     console.error(error);
-   res.status(500).json({ error: error.message || 'Internal Server Error' });
+    return res.status(500).json({ error: error.message ,data: error.data });
   }
   },
 };
