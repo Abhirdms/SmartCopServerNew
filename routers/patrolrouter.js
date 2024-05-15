b05284756a9950b2fa1587ef17baeeea2a34b1e2
mishrakhushi16 <132750159+mishrakhushi16@users.noreply.github.com>
2024-05-15 13:58:19 +0530

    Update patrolrouter.js

--- routers/patrolrouter.js
+++ routers/patrolrouter.js
@@ -16,4 +16,33 @@ router.get('/get_destinations', async (req, res) => {
   }
 });
 
+
+router.put('/patrolmarkstatus', async (req, res) => {
+  try {
+    const { destinationId, marked } = req.body;
+    console.log(req.body);
+
+    // Find the destination by ID and update its marked status
+    const updatedDestination = await Destination.findOneAndUpdate(
+      { "destinations._id": destinationId },
+      { $set: { "destinations.$.marked": marked } },
+      { new: true }
+    );
+
+    if (!updatedDestination) {
+      console.log("Not found");
+      return res.status(404).json({ message: 'Destination not found' });
+      
+    }
+
+    res.status(200).json({ updatedDestination });
+    console.log("Updated succesffully");
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+
 module.exports = router;
