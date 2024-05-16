bd45736fd46913bf9da2971431d79a44847ec509
mishrakhushi16 <132750159+mishrakhushi16@users.noreply.github.com>
2024-05-16 15:20:01 +0530

    Update searchrouter.js

--- routers/searchrouter.js
+++ routers/searchrouter.js
@@ -66,4 +66,19 @@ router.post('/updateJailRelease', async (req, res) => {
   }
 });
 
+router.get('/userDetails/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const user = await MyModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 module.exports = router;
