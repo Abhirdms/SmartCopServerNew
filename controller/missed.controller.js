8c0f011a672983cff74d130b58d72da594aecf5b
mishrakhushi16 <132750159+mishrakhushi16@users.noreply.github.com>
2024-05-22 14:29:39 +0530

    Update missed.controller.js

--- controller/missed.controller.js
+++ controller/missed.controller.js
@@ -3,20 +3,35 @@ const uploadService = require('../services/missed.service');
 module.exports = {
   uploadFile: async (req, res) => {
     try {
-      if (!req.file) {
-        return res.status(400).json({ error: 'No file provided' });
-      }
-
-      const result = await uploadService.processUpload(req.file);
-
-     if (result.message) {
-        return res.status(200).json({ message: result.message, data: result.data });
+        if (req.file) {
+        // If a file is provided, it's an Excel upload
+        const result = await uploadService.processUpload(req,req.file);
+        return { message: result.message, data: result.data };
+      } else if (req.body) {
+        // If req.body exists, it's individual data submission
+        const result = await uploadService.processUpload(req);
+        return result;
       } else {
-        return res.status(500).json({ error: 'File upload failed' });
+        return { error: 'No file or data provided' };
       }
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ error: error.message || 'Internal Server Error' });
+      res.status(500).json({ error: error.message || 'Internal Server Error' });
     }
+    //   if (!req.file) {
+    //     return res.status(400).json({ error: 'No file provided' });
+    //   }
+
+    //   const result = await uploadService.processUpload(req.file);
+
+    //  if (result.message) {
+    //     return res.status(200).json({ message: result.message, data: result.data });
+    //   } else {
+    //     return res.status(500).json({ error: 'File upload failed' });
+    //   }
+    // } catch (error) {
+    //   console.error(error);
+    //   return res.status(500).json({ error: error.message || 'Internal Server Error' });
+    // }
   },
 };
