2589f54717ad6515b830c0fd396341e8d43cf57c
mishrakhushi16 <132750159+mishrakhushi16@users.noreply.github.com>
2024-06-05 13:23:25 +0530

    Update visitor.js

--- model/visitor.js
+++ model/visitor.js
@@ -1,14 +1,41 @@
 
 const mongoose = require('mongoose');
 
+const visitorreportSchema = new mongoose.Schema({
+  visitorName: {
+    type: String,
+  },
+  designation: {
+    type: String,
+  },
+  visitingDate: {
+    type: Date,
+  },
+  duration: {
+    type: String,
+  },
+  highQualityPhotographs: {
+    type: String,// If this field is not always present, you can set it to optional
+  }
+});
+
 const visitorSchema = new mongoose.Schema({
-  name: String,
-  designation: String,
-  uploadedFiles: [String],
-  isVisited: { type: Boolean, default: false },
-  crimeNumber: String,
+  offenderId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  offenderName: {
+    type: String,
+  },
+  crimeHead: {
+    type: String,
+  },
+  visitors: [visitorreportSchema] // Array of visitor objects
 });
 
 const Visitor = mongoose.model('Visitor', visitorSchema);
 
 module.exports = Visitor;
+
+
